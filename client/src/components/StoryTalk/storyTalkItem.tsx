2807a2ae9c02db0bb4ab3db09255a6da0208ff97
Hans Jo <wngkd441@gmail.com>
2022-03-23 14:20:44 +0900

    FEAT: ✨ 사연 대화 삭제 컴포넌트

--- client/src/components/StoryTalk/storyTalkItem.tsx
+++ client/src/components/StoryTalk/storyTalkItem.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import axios from "axios";
+import { useRadioGroup } from "@mui/material";
 
 export default function StoryTalkItem({ storyTalk }: any) {
+  // 삭제 요청
+  const DeleteStoryTalk = async () => {
+    await axios
+      .post("/api/request", { requestId: useRadioGroup, requestType: "delete" })
+      .then((response: any) => {
+        console.log("response: ", response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       <div>{storyTalk.story.senderId}</div>
+      <button onClick={DeleteStoryTalk}>사연 대화 삭제</button>
       <div>{storyTalk.story.image}</div>
     </>
   );
