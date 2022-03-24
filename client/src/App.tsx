d293ed5b3873fe926c06fe84d6c920cbdd24ddab
breathinlee <dltnals7350@naver.com>
2022-03-24 16:04:55 +0900

    CHORE: � 프로필 svg 파일 및 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./pages/User/Register";
 import ChangePassword from "./components/User/Profile/DeleteAccount";
 import BottomNavbar from "./components/BottomNavbar";
+import Profile from "./pages/User/Profile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/change-password" element={<ChangePassword />} />
+        <Route path="/profile" element={<Profile />}></Route>
       </Routes>
       <BottomNavbar />
     </>
