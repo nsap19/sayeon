dad35a2714aed10fa9ee477efb7f0c4ff62384d8
이혜민 <viphyemin@gmail.com>
2022-03-24 11:49:51 +0900

    CHORE: 📝 webSecurity 설정 추가

--- server/src/main/java/com/ssafy/sayeon/common/config/WebSecurityConfig.java
+++ server/src/main/java/com/ssafy/sayeon/common/config/WebSecurityConfig.java
@@ -54,6 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests().antMatchers(
                 		"/users/login",
+                		"/users/signup",
                          "/",
                          "/v2/api-docs",           // swagger
                          "/webjars/**",            // swagger-ui webjars
