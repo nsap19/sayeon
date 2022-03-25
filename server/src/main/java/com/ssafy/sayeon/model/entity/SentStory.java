621186206505dca4f080e6b928fb2bbbabee45e9
sny3238 <sny3238@gmail.com>
2022-03-25 11:02:25 +0900

    CHORE:: ✨ 빋은 사연, 보낸 사연 엔티티 추가

---
+++ server/src/main/java/com/ssafy/sayeon/model/entity/SentStory.java
@@ -0,0 +1,58 @@
+package com.ssafy.sayeon.model.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import net.bytebuddy.utility.RandomString;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name="sentstory")
+public class SentStory {
+
+    @Id
+    @GeneratedValue(generator="uuid")
+	@GenericGenerator(name="uuid", strategy = "uuid2")
+	@Column(name="storyId", nullable=false, unique=true, length=100,columnDefinition = "BINARY(16)")
+    private String storyId;
+
+    @Column(name="senderId", length=100, nullable=false)
+    private String senderId;
+    
+    @Column(name="dateSent", length=100, nullable=false)
+    private String dateSent;
+    
+    @Column(name="image", length=300, nullable=false)
+    private String image;
+
+	@Column(name="watingId", nullable=false)
+    private int watingId;
+	
+	@OneToOne(mappedBy="member")
+	ReceivedStrory receivedStory; //읽기 전용 필드
+
+
+}
