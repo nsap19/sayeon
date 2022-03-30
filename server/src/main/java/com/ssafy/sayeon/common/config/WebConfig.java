930bf55169fd03e8ab8d3595e7e1d44953b60d6f
이혜민 <viphyemin@gmail.com>
2022-03-30 15:18:52 +0900

    FIX: 🐛 Cors HTTP method 허용 문제 해결

--- server/src/main/java/com/ssafy/sayeon/common/config/WebConfig.java
+++ server/src/main/java/com/ssafy/sayeon/common/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins("http://localhost:3000")
-        .maxAge(3600); // 3600초 동안 preflight 결과를 캐시에 저장
+        .maxAge(3600) // 3600초 동안 preflight 결과를 캐시에 저장
+        .allowedMethods("*");
   }
 }
