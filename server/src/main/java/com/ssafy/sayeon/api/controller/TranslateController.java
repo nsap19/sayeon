cc3c63938e683eeab5a8b7d1ed5bc7ea3edb17cd
이혜민 <viphyemin@gmail.com>
2022-04-02 17:53:59 +0900

    STYLE: 🎨 미사용 import 제거

--- server/src/main/java/com/ssafy/sayeon/api/controller/TranslateController.java
+++ server/src/main/java/com/ssafy/sayeon/api/controller/TranslateController.java
@@ -1,39 +1,22 @@
 package com.ssafy.sayeon.api.controller;
 
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.sayeon.api.request.KeywordsReq;
-import com.ssafy.sayeon.api.request.SayeonReq;
-import com.ssafy.sayeon.api.response.AdvancedResponseBody;
-import com.ssafy.sayeon.api.response.BaseResponseBody;
-import com.ssafy.sayeon.api.response.ReceivedStoryInfo;
 import com.ssafy.sayeon.api.response.TranslatedKeywordsBody;
-import com.ssafy.sayeon.api.service.JwtUserDetailsService;
-import com.ssafy.sayeon.api.service.MyInfoService;
-import com.ssafy.sayeon.api.service.StoryListService;
 import com.ssafy.sayeon.api.service.TranslationService;
-import com.ssafy.sayeon.common.util.JwtTokenUtil;
-import com.ssafy.sayeon.model.entity.Member;
-import com.ssafy.sayeon.model.entity.ReceivedStory;
-import com.ssafy.sayeon.model.entity.SentStory;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
