ed68f3822d3d8bde63089a2150edfa858a663d77
kang su hyun <kangsu02@gmail.com>
2022-04-02 17:09:05 +0900

    CHORE: 📝 닉네임 길이 제한 변경

--- client/src/components/User/Register/NicknameController.tsx
+++ client/src/components/User/Register/NicknameController.tsx
@@ -54,8 +54,8 @@ const NicknameController: React.FC<{
           message: "닉네임을 입력해주세요.",
         },
         maxLength: {
-          value: 10,
-          message: "닉네임은 10자 이내로 입력해주세요.",
+          value: 8,
+          message: "닉네임은 8자 이내로 입력해주세요.",
         },
         validate: () => {
           if (!validatedNickname) {
