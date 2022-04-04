202d16737cf35025fb6be6cb6eda9d155cf2ed57
kang su hyun <kangsu02@gmail.com>
2022-04-04 18:55:11 +0900

    FEAT: ✨ 메인페이지 최근 받은 사연 없는 경우
    메인페이지 최근 받은 사연 없는 경우 렌더링 컴포넌트 추가
    디자인 수정

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -57,14 +57,13 @@ const Main: React.FC = () => {
         direction="column"
         justifyContent="flex-end"
         alignItems="center"
-        spacing={1.5}
-        sx={{ height: "calc(100% - 56px)", padding: "20px 0 10px" }}
+        sx={{ height: "calc(100% - 70px)" }}
       >
         <Logo style={{ width: "60%", height: "auto" }} />
 
         <Box
           sx={{
-            width: "85%",
+            width: "80%",
             backgroundColor: "white",
             borderRadius: "20px",
             boxShadow: "0px 10px 30px rgba(0, 0, 0, 0.05)",
@@ -72,10 +71,12 @@ const Main: React.FC = () => {
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
+            margin: "30px 20px 20px",
           }}
         >
-          <p style={{ padding: "40px 40px 20px" }}>
-            당신의 사연을 사진에 담아 보내고 비슷한 사연을 답장으로 받아보세요.
+          <p style={{ padding: "40px 40px 15px", textAlign: "center" }}>
+            <p>사진으로 전하는 당신의 사연,</p>
+            <p>지금 보내보세요.</p>
           </p>
           <Button
             href="/send"
@@ -105,15 +106,22 @@ const Main: React.FC = () => {
           <Stack
             direction="row"
             justifyContent="space-between"
+            alignItems="end"
             sx={{
               width: "85%",
-              margin: "10px 0",
+              margin: "10px",
             }}
           >
             <p>최근 받은 사연</p>
-            <Link href="/story-list" underline="none">
-              더보기
-            </Link>
+            {recentStories && (
+              <Link
+                href="/story-list"
+                underline="none"
+                sx={{ fontSize: "14px" }}
+              >
+                더보기
+              </Link>
+            )}
           </Stack>
           <Box
             sx={{
@@ -123,27 +131,42 @@ const Main: React.FC = () => {
               justifyContent: "end",
             }}
           >
-            <Stack
-              direction="row"
-              justifyContent=""
-              alignItems="center"
-              spacing={2}
-              sx={{ width: "92%", overflowX: "auto", height: "100%" }}
-            >
-              {recentStories.map((recentStory) => (
-                <Box
-                  sx={{ width: "auto", height: "90%" }}
-                  key={recentStory.storyId}
-                >
-                  <Polaroid
-                    imageUrl={recentStory.image}
-                    imageType={recentStory.imageType}
-                    senderNickname={recentStory.senderNickname}
-                    dateReceived={recentStory.dateReceived}
-                  />
-                </Box>
-              ))}
-            </Stack>
+            {recentStories ? (
+              <Stack
+                direction="row"
+                justifyContent=""
+                alignItems="start"
+                spacing={2}
+                sx={{ width: "92%", overflowX: "auto", height: "100%" }}
+              >
+                {recentStories.map((recentStory) => (
+                  <Box
+                    sx={{ width: "auto", height: "90%" }}
+                    key={recentStory.storyId}
+                  >
+                    <Polaroid
+                      imageUrl={recentStory.image}
+                      imageType={recentStory.imageType}
+                      senderNickname={recentStory.senderNickname}
+                      dateReceived={recentStory.dateReceived}
+                    />
+                  </Box>
+                ))}
+              </Stack>
+            ) : (
+              <p
+                style={{
+                  alignSelf: "center",
+                  margin: "0 auto",
+                  textAlign: "center",
+                  color: "#8c8888",
+                  fontSize: "14px",
+                }}
+              >
+                <p>아직 주고 받은 사연이 없습니다.</p>
+                <p>지금 바로 사연을 보내보세요!</p>
+              </p>
+            )}
           </Box>
         </Box>
       </Stack>
