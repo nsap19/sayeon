125fe21ba011ce18763701d3392b07dc8b0bde86
Hans Jo <wngkd441@gmail.com>
2022-04-04 14:44:21 +0900

    FEAT: ✨ 사연 대화 컴포넌트 연결

--- client/src/components/StoryTalk/StoryTalkItem.tsx
+++ client/src/components/StoryTalk/StoryTalkItem.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Route, useNavigate } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { Button, Box, Stack } from "@mui/material";
 import StoryTalk from "pages/StoryTalk/StoryTalk";
 import Polaroid from "components/Story/Polaroid";
-import { ReactComponent as More } from "../../assets/icon/more.svg";
 
 interface Story {
   storyId: number;
@@ -19,7 +18,6 @@ interface Story {
 
 export default function StoryTalkItem({ storyTalk, myInfo }: any) {
   // STATE
-  const navigate = useNavigate();
   const firstId = myInfo.userId;
   const secondId = () => {
     if (firstId === storyTalk[0].senderId) {
@@ -30,7 +28,9 @@ export default function StoryTalkItem({ storyTalk, myInfo }: any) {
   const [otherUserInfo, setOtherUserInfo] = useState<{
     nickname: string;
     profilePic: number;
+    withdrawal: boolean;
   }>();
+  const [open, setOpen] = useState(false);
 
   // 정보 요청
 
@@ -46,6 +46,7 @@ export default function StoryTalkItem({ storyTalk, myInfo }: any) {
         setOtherUserInfo({
           nickname: res.data.data.memberProfile.nickname,
           profilePic: res.data.data.memberProfile.profilePic,
+          withdrawal: res.data.data.memberProfile.withdrawal,
         });
       })
       .catch((err) => console.log(err));
@@ -56,52 +57,58 @@ export default function StoryTalkItem({ storyTalk, myInfo }: any) {
   }, []);
 
   const openStoryTalk = () => {
-    navigate("/story-talk/:nickname");
+    setOpen(true);
   };
 
   return (
-    <Box>
-      <Stack direction="column" alignItems="start" spacing={2}>
-        <Button
-          variant="text"
-          sx={{ color: "black" }}
-          href="/story-talk/:nickname"
-        >
-          {otherUserInfo && (
-            <img
-              src={require(`../../assets/images/profile/Avatars-${otherUserInfo?.profilePic}.png`)}
-              alt="profile pic"
-              style={{ width: "30px", marginRight: "10px" }}
-            />
-          )}
-          {otherUserInfo?.nickname}
-        </Button>
-        <Box
-          sx={{
-            width: "40%",
-            height: "40%",
-            display: "flex",
-            justifyContent: "start",
-          }}
-        >
-          <Stack direction="row" justifyContent="start" spacing={2}>
-            {storyTalk.map(
-              (story: any) =>
-                otherUserInfo && (
-                  <div key={story.storyId} onClick={openStoryTalk}>
-                    <Polaroid
-                      imageUrl={story.image}
-                      imageType={story.imageType}
-                      senderNickname={otherUserInfo.nickname}
-                      dateReceived={story.dateReceived}
-                    />
-                  </div>
-                )
-            )}
+    <>
+      {open ? (
+        <Box>
+          <Stack direction="column" alignItems="start" spacing={2}>
+            <div onClick={openStoryTalk}>
+              {otherUserInfo && (
+                <img
+                  src={require(`../../assets/images/profile/Avatars-${otherUserInfo?.profilePic}.png`)}
+                  alt="profile pic"
+                  style={{ width: "30px", marginRight: "10px" }}
+                />
+              )}
+              {otherUserInfo?.nickname}
+            </div>
+            <Box
+              sx={{
+                width: "40%",
+                height: "40%",
+                display: "flex",
+                justifyContent: "start",
+              }}
+            >
+              <Stack direction="row" justifyContent="start" spacing={2}>
+                {storyTalk.map(
+                  (story: any) =>
+                    otherUserInfo && (
+                      <div key={story.storyId} onClick={openStoryTalk}>
+                        <Polaroid
+                          imageUrl={story.image}
+                          imageType={story.imageType}
+                          senderNickname={otherUserInfo.nickname}
+                          dateReceived={story.dateReceived}
+                        />
+                      </div>
+                    )
+                )}
+              </Stack>
+            </Box>
           </Stack>
+          <hr style={{ margin: "7% 5% 7%" }} />
         </Box>
-      </Stack>
-      <hr style={{ margin: "7% 5% 7%" }} />
-    </Box>
+      ) : (
+        <StoryTalk
+          myInfo={myInfo}
+          otherUserInfo={otherUserInfo}
+          setOpen={setOpen}
+        />
+      )}
+    </>
   );
 }
