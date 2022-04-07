73d5d16d90f48800c7be1bf84e9faafb7d00040d
kang su hyun <kangsu02@gmail.com>
2022-04-07 10:34:14 +0900

    STYLE: 🎨 폴라로이드 열람시간양식 변경

--- client/src/components/Polaroid/KeywordsPolaroid.tsx
+++ client/src/components/Polaroid/KeywordsPolaroid.tsx
@@ -73,9 +73,8 @@ const Polaroid: React.FC<{
 
   const hidden = new Date().getTime() < new Date(dateReceived).getTime();
 
-  const hourDifference = Math.round(
-    (new Date(dateReceived).getTime() - new Date().getTime()) / 36e5
-  );
+  const hourDifference =
+    (new Date(dateReceived).getTime() - new Date().getTime()) / 36e5;
 
   return (
     <>
@@ -103,14 +102,14 @@ const Polaroid: React.FC<{
 
         {hidden && (
           <HiddenAlert>
-            {hourDifference > 0 ? (
+            {hourDifference >= 1 ? (
               <>
-                <p>{hourDifference}시간 뒤에</p>
+                <p>{Math.round(hourDifference)}시간 뒤에</p>
                 <p>사연이 열립니다.</p>
               </>
             ) : (
               <>
-                <p>1시간 이내로</p>
+                <p>{Math.round(hourDifference * 60)}분 뒤에</p>
                 <p>사연이 열립니다.</p>
               </>
             )}
